6cddcc7a70c901054cdb64e5a62c7304ef7a23ba
parking-been <ysbeen00@gmail.com>
2025-10-23 17:26:48 +0900

    feat(OutputView) : 자동차 이름 입력 멘트 출력 기능 추가

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,9 @@
+package racingcar.view;
+
+public class OutputView {
+    public static final String INPUT_MESSAGE_OF_NAME_OF_CARS = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    public static void printInputMessageOfNameOfCars(){
+        System.out.println(INPUT_MESSAGE_OF_NAME_OF_CARS);
+    }
+}
