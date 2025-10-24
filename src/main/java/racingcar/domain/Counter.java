383089f716a8ee2590e03e90b30e97161ee7697d
parking-been <ysbeen00@gmail.com>
2025-10-24 11:15:33 +0900

    feat(Racing,Counter) : 서기를 통해 남은 횟수를 얻은 후 경기를 계속 진행해야하는지 여부 판단하는 기능 추가

--- src/main/java/racingcar/domain/Counter.java
+++ src/main/java/racingcar/domain/Counter.java
@@ -1,8 +1,19 @@
 package racingcar.domain;
 
 public class Counter {
+
     private int totalCount;
+
     public Counter(int totalCount) {
         this.totalCount = totalCount;
     }
+
+    public void decreaseCount(){
+        totalCount--;
+    }
+
+    public int getTotalCount() {
+        return totalCount;
+    }
+
 }
--- src/main/java/racingcar/domain/Racing.java
+++ src/main/java/racingcar/domain/Racing.java
@@ -16,12 +16,29 @@ public class Racing {
         this.racingResult = racingResult;
     }
 
+    /**
+     * 자동차들이 이동할 것을 명령
+     */
     public void orderCarsToMove(){
+        //자동차 이동
         cars.moveCars();
+        //한턴 끝남
+        counter.decreaseCount();
     }
 
     private void orderCarsLocation(){
 
+    }
+
+
+    /**
+     * 경기를 계속 진행해야하는지 여부 판단
+     */
+    public boolean canProceedRace(){
+        if (counter.getTotalCount()<=0){
+            return false;
+        }
+        return true;
     }
 
 }
