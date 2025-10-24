314e107f85c6f0771b5297f67bbf8f723a5d0e21
parking-been <ysbeen00@gmail.com>
2025-10-24 11:15:50 +0900

    test(Racing) : 남은 횟수에 따라 경기를 진행할 수 있는지 없는지 결정하는 기능 테스트 추가

--- src/test/java/racingcar/domain/RacingTest.java
+++ src/test/java/racingcar/domain/RacingTest.java
@@ -1,8 +1,48 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RacingTest {
 
+    Racing racing;
+
+    @BeforeEach
+    public void beforeEach(){
+        Cars cars = new Cars(List.of("pobi","mini"));
+        Counter counter = new Counter(3);
+        RacingResult racingResult = new RacingResult();
+        racing = new Racing(cars, counter, racingResult);
+    }
+
+    @AfterEach
+    public void afterEach(){
+
+    }
+
+    @Test
+    @DisplayName("남은 횟수가 경기를 진행할 수 있을 때")
+    void 경기_진행여부_판단(){
+        racing.orderCarsToMove();
+        racing.orderCarsToMove();
+        assertThat(racing.canProceedRace()).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("남은 횟수가 경기를 진행할 수 없을 때")
+    void 경기_진행여부_판단_2(){
+        racing.orderCarsToMove();
+        racing.orderCarsToMove();
+        racing.orderCarsToMove();
+        assertThat(racing.canProceedRace()).isEqualTo(false);
+
+    }
 
 }
